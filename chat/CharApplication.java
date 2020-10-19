8affe6d41b7fbce7b89899f93a6dc61de7bf4371
Denis Bizhanov <61880748+AmigoSnegr@users.noreply.github.com>
2020-10-19 15:20:27 +0200

    Add files via upload

---
+++ chat/CharApplication.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class CharApplication {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                new Socket("localhost", 8554);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        new Thread(() -> {
+            try {
+                new Socket("localhost", 8554);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        new Thread(() -> {
+            try {
+                new Socket("localhost", 8554);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        new Thread(() -> {
+            try {
+                new Socket("localhost", 8554);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
